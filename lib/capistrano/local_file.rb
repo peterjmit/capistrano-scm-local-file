9c214d55a1f55f33a37227d436074e4be1ae96de
Peter Mitchell <peterjmit@gmail.com>
2014-10-21 18:32:22 -0400

    Rsync with -a flag. Clean local_file directory before upload

--- lib/capistrano/local_file.rb
+++ lib/capistrano/local_file.rb
@@ -24,13 +24,14 @@ class Capistrano::LocalFile < Capistrano::SCM
 
     # Upload a local file to the server
     def update
+      context.execute :rm, '-rf', "#{deploy_path}/local_file/*"
       context.upload! fetch(:repo_url), "#{deploy_path}/local_file/#{fetch(:repo_url)}"
     end
 
     # Unpack and rsync the contents into release path
     def release
       context.execute :tar, '-xvzf', "#{deploy_path}/local_file/#{fetch(:repo_url)}"
-      context.execute :rsync, "-r", "--exclude=#{fetch(:repo_url)}", "#{deploy_path}/local_file/*", release_path
+      context.execute :rsync, "-a", "--exclude=#{fetch(:repo_url)}", "#{deploy_path}/local_file/*", release_path
     end
 
     # TODO: SHA of archive, or variable or something to set the revision number?
